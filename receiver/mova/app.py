c020ee089ea659fa0d1f090a607985569dc66fcf
Joshy Cyriac <j.cyriac@gmail.com>
2019-11-05 17:06:04 +0100

    job history added

--- receiver/mova/app.py
+++ receiver/mova/app.py
@@ -25,6 +25,7 @@ js = Bundle(
     "js/jquery-3.3.1.min.js",
     "js/bootstrap.bundle.min.js",
     "js/jquery.noty.packaged.min.js",
+    "js/intercooler.js",
     "js/script.js",
     filters="jsmin",
     output="gen/packed.js",
@@ -32,10 +33,53 @@ js = Bundle(
 assets.register("js_all", js)
 
 
+@app.template_filter("to_date")
+def to_date(timestamp):
+    if timestamp:
+        return datetime.fromtimestamp(int(timestamp)).strftime("%d.%m.%Y %H:%M:%S")
+    else:
+        return ""
+
+
 @app.route("/")
 def main():
-    files = list(Path("jobs").glob("*.json"))
+    files = [i.stem for i in Path("jobs").glob("*.json")]
     return render_template("index.html", version=version, files=files)
+
+
+@app.route("/show")
+def show():
+    filename = request.args.get("filename")
+    with app.open_resource(f"../jobs/{filename}.json") as f:
+        content = json.load(f)
+    return render_template(
+        "show.html",
+        version=version,
+        filename=filename,
+        content=json.dumps(content, indent=4),
+    )
+
+
+@app.route("/resend")
+def resend():
+    filename = request.args.get("filename")
+    parts = filename.split("_")
+    with app.open_resource(f"../jobs/{filename}.json") as f:
+        data = json.load(f)
+    if parts[1] == "download":
+        series_list = data.get("data")
+        dir_name = data.get("dir")
+        app.logger.info("download called and saving to %s", dir_name)
+        length = download_series(app.config, series_list, dir_name)
+        return render_template("success.html")
+    elif parts[1] == "transfer":
+        target = data.get("target", "")
+        series_list = data.get("data", "")
+        app.logger.info("transfer called and sending to %s", target)
+        length = transfer_series(app.config, series_list, target)
+        return render_template("success.html")
+    else:
+        return render_template("success.html")
 
 
 @app.route("/download", methods=["POST"])
@@ -48,6 +92,7 @@ def download():
         json.dump(data, f)
     series_list = data.get("data")
     dir_name = data.get("dir")
+    app.logger.info("download called and saving to %s", dir_name)
     length = download_series(app.config, series_list, dir_name)
     return json.dumps({"status": "OK", "series_length": length})
 
