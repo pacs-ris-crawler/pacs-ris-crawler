34addcac7e303723bb9530cd7e64477f4cb2b481
Joshy Cyriac <j.cyriac@gmail.com>
2020-03-09 13:07:07 +0100

    removed obsolete code

--- crawler/crawler/query.py
+++ crawler/crawler/query.py
@@ -32,7 +32,9 @@ def query_for_study_uid(config, accession_number):
             ids.append(r["StudyInstanceUID"])
         return ids
     raise LookupError(
-        "No result found for accession number: {}\nQuery was: {}".format(accession_number, query)
+        "No result found for accession number: {}\nQuery was: {}".format(
+            accession_number, query
+        )
     )
 
 
@@ -55,48 +57,6 @@ def get_months_of_year(year: str) -> List[Dict[str, str]]:
     start, end = year_start_end(year)
     # MS is month start frequency
     return [d.strftime("%Y-%m") for d in pd.date_range(start, end, freq="MS")]
-
-
-def query_month(config, year_month: str) -> List[Dict[str, str]]:
-    start = datetime.datetime.strptime(year_month, "%Y-%m")
-    end = start + pd.tseries.offsets.MonthEnd()
-    results = []
-    for day in pd.date_range(start, end):
-        for mod in modalities():
-            results.extend(query_day_extended(config, mod, day, INITIAL_TIME_RANGE))
-    return results
-
-
-def query_day(config, day: str) -> List[Dict[str, str]]:
-    query_date = datetime.datetime.strptime(day, "%Y-%m-%d")
-    results = []
-    for mod in modalities():
-        results.extend(query_day_extended(config, mod, query_date, INITIAL_TIME_RANGE))
-    return results
-
-
-def query_day_extended(
-    config, mod: str, day: datetime.datetime, time_range: str
-) -> List[Dict[str, str]]:
-    query = prepare_query(config, mod, day, time_range)
-    result, size = run(query)
-    limit = int(config["SERIES_LIMIT"])
-    if size < limit:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        return [result]
-    else:
-        sys.stdout.write("|")
-        sys.stdout.flush()
-        logging.debug(
-            "results >= {} for {} {} {}, splitting".format(
-                str(limit), mod, day, time_range
-            )
-        )
-        l, r = split(time_range)
-        return query_day_extended(config, mod, day, l) + query_day_extended(
-            config, mod, day, r
-        )
 
 
 def query_day_accs(config, day) -> List[Dict[str, str]]:
