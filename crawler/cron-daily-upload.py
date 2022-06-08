d6d976299ffa58fa86635cdd62c57e5626729143
Joshy Cyriac <j.cyriac@gmail.com>
2022-06-08 11:35:06 +0200

    add option for specific date

--- crawler/cron-daily-upload.py
+++ crawler/cron-daily-upload.py
@@ -1,19 +1,22 @@
-from calendar import week
+import datetime
 from datetime import datetime
 
-import datetime
-import requests
 import click
+import requests
 
 
 @click.command()
 @click.option("--host", help="Hostname to connect to, defaults to localhost")
 @click.option("--port", help="Port to connect to ")
-def upload(host, port):
+@click.option("--day", help="Specific day to upload, format is: YYYY-mm-dd")
+def upload(host, port, day):
     print(f"Calling url: http://{host}:{port}")
-    today = datetime.date.today()
-    week_ago = today - datetime.timedelta(days=7)
-    week_ago_str = week_ago.strftime("%Y-%m-%d")
+    if not day:
+        today = datetime.date.today()
+        week_ago = today - datetime.timedelta(days=7)
+        week_ago_str = week_ago.strftime("%Y-%m-%d")
+    else:
+        week_ago_str = day
     payload = {"from-date": week_ago_str, "to-date": week_ago_str}
     print(f"Payload is: {payload}")
     r = requests.get(f"http://{host}:{port}/batch-upload", params=payload)
